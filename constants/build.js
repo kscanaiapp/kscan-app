bcdcfab6f0a7d36129d5ed8b7a4ee53ada852780
Justin Smith <justin.landes@gmail.com>
2026-07-07 15:45:51 -0400

    chore(release): sync store.config.json and build label to v1.0.1

--- constants/build.js
+++ constants/build.js
@@ -1,4 +1,4 @@
-export const APP_BUILD_LABEL = 'Android Beta v1.5.2';
+export const APP_BUILD_LABEL = 'v1.0.1';
 export const DEV_FALLBACK_STATUS = 'false';
 export const QA_TOOLS_ENABLED =
   typeof __DEV__ !== 'undefined' && __DEV__ === true;
